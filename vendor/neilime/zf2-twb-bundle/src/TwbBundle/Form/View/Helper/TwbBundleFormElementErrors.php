c3d2860b820cf36f2ef527f092170f155a11fdd5
Jesse Harlan <gotsanity@gmail.com>
2014-09-07 12:16:30 -0700

    removal of vendor libs that are not in use

--- vendor/neilime/zf2-twb-bundle/src/TwbBundle/Form/View/Helper/TwbBundleFormElementErrors.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace TwbBundle\Form\View\Helper;
-class TwbBundleFormElementErrors extends \Zend\Form\View\Helper\FormElementErrors{
-    protected $attributes = array(
-        'class' => 'help-block'
-    );
-}
